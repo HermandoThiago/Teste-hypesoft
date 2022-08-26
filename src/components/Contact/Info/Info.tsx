eecebe16ee476a12e1ecceb5244cca09a2e47b71
HermandoThiago <hermandodev@gmail.com>
2022-08-26 15:15:51 -0300

    fix: resolucao de bug no component Info mobile

--- src/components/Contact/Info/Info.tsx
+++ src/components/Contact/Info/Info.tsx
@@ -10,7 +10,7 @@ export default function Info({ children, icon }: IPropsInfo){
     return(
         <StyledInfo width={100} direction='row' py={10} px={10}>
             <CircleIcon>{icon}</CircleIcon>
-            <Box width={80} direction='column' content='center' px={20}>
+            <Box width={75} direction='column' content='center' px={20}>
                 {children}
             </Box>
         </StyledInfo>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import { Link } from 'react-scroll';
 export default function Header(){
     return(
         <StyledHeader>
-            <Box width={36} py={25} direction='row'>
+            <Box width={36} py={25} direction='row' content='space'>
             <Link to="description" spy={true} smooth={true} offset={50} duration={500}><Typography size="small" color="primary">ABOUT</Typography></Link>
             <Link to="services" spy={true} smooth={true} offset={50} duration={500}><Typography size="small" color="primary">SERVICES</Typography></Link>
             <Link to="works" spy={true} smooth={true} offset={50} duration={500}><Typography size="small" color="primary">WORKS</Typography></Link>
